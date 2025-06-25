5269151c818777d47bac144384d9723b75562616
Daniel Jordan <djordanvillazala@gmail.com>
2025-06-25 19:07:33 +0200

    El tipo de cámara queda registrado en registro_temperaturas.csv

--- utils/registro_temperatura.py
+++ utils/registro_temperatura.py
@@ -16,11 +16,13 @@ def guardar_temperatura(camara: str, temperatura: float) -> bool:
     fecha = now.strftime("%Y-%m-%d")
     hora = now.strftime("%H:%M")
 
-    # Validación de tipo de cámara
     with open(CSV_CAMARAS, newline='', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        next(reader)
-        nombres_camaras = [row[0] for row in reader]
+        reader = csv.DictReader(f)
+        camaras_dict = {row['Nombre']: row['Tipo'] for row in reader}
+        nombres_camaras = list(camaras_dict.keys())
+        
+
+    # Validación de tipo de cámara
     if camara not in nombres_camaras:
         print("⚠️ Tipo de cámara inválido.")
         return False
@@ -30,6 +32,9 @@ def guardar_temperatura(camara: str, temperatura: float) -> bool:
         print("⚠️ Temperatura fuera de rango aceptable.")
         return False
     
+    # Recoge el tipo de cámara asociado al nombre
+    tipo_camara = camaras_dict[camara]
+    
     # Asegurar existencia del directorio
     os.makedirs(os.path.dirname(CSV_REGISTROS), exist_ok=True)
 
@@ -37,8 +42,8 @@ def guardar_temperatura(camara: str, temperatura: float) -> bool:
     with open(CSV_REGISTROS, mode='a', newline='', encoding='utf-8') as archivo:
         writer = csv.writer(archivo)
         if archivo.tell() == 0:
-            writer.writerow(['Fecha', 'Hora', 'Cámara', 'Temperatura'])
-        writer.writerow([fecha, hora, camara, temperatura])
+            writer.writerow(['Fecha', 'Hora', 'Camara', 'Temperatura', 'TipoCamara'])
+        writer.writerow([fecha, hora, camara, temperatura, tipo_camara])
 
     print(f'✅ Registro guardado: {fecha} {hora} - {camara} - {temperatura}ºC')
     return True
